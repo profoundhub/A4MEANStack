ea7453a2af378adeb39d19e8fb93ca0728a2eb30
Daniel Lim <hbt@markhamboards.com>
2018-04-03 21:00:44 -0400

    update .ts files

--- mean/src/app/app.module.ts
+++ mean/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+
+// Import the Http Module and our Data Service
+import { HttpModule } from '@angular/http';
+import { DataService } from './data.service';
+
 
 
 @NgModule({
